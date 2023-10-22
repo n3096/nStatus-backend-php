574407f2ae9996bd3ab7a8f6fcf792dec844ea8a
Philipp Wilke <philipp.wilke@stud.leuphana.de>
2023-10-23 00:24:53 +0200

    add configuration loading and adjust framework directories

--- model/Service.php
+++ model/Service.php
@@ -3,17 +3,23 @@
 namespace model;
 
 class Service {
+    public string $id;
     public string $name;
     public string $hostName;
     public SocketProtocol $socketProtocol;
     public int $port;
-    public ?int $timeout;
+    public string $icon;
+    public bool $enabled;
+    public int $timeout;
 
-    public function __construct(string $name, string $hostName, SocketProtocol $socketProtocol, int $port, ?int $timeout = null) {
+    public function __construct(?string $id, string $name, string $hostName, SocketProtocol $socketProtocol, int $port, string $icon, bool $enabled = true, int $timeout = 10) {
+        $this->id = $id ?: uniqid();
         $this->name = $name;
         $this->hostName = $hostName;
         $this->socketProtocol = $socketProtocol;
         $this->port = $port;
+        $this->icon = $icon;
+        $this->enabled = $enabled;
         $this->timeout = $timeout;
     }
 }
--- model/SocketProtocol.php
+++ model/SocketProtocol.php
@@ -2,9 +2,26 @@
 
 namespace model;
 
+use InvalidArgumentException;
+
 enum SocketProtocol: string {
     case TCP = "TCP";
     case HTTP = "HTTP";
     case HTTPS = "HTTPS";
     case UDP = "UDP";
+
+    public static function parse(string $socketProtocol) {
+        switch ($socketProtocol) {
+            case self::TCP->value:
+                return self::TCP;
+            case self::HTTP->value:
+                return self::HTTP;
+            case self::HTTPS->value:
+                return self::HTTPS;
+            case self::UDP->value:
+                return self::UDP;
+            default:
+                throw new InvalidArgumentException("Invalid SocketProtocol: '$socketProtocol'");
+        }
+    }
 }
